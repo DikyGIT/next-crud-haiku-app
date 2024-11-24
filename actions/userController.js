3d608b3823958161a0eab83eac26270e04cd44f8
DikyGIT <dikyoga27@gmail.com>
2024-11-24 14:31:26 +0700

    add username and password validation

---
+++ actions/userController.js
@@ -0,0 +1,68 @@
+"use server";
+
+function isAlphaNumeric(x) {
+  const regex = /^[a-zA-Z0-9]*$/;
+  return regex.test(x);
+}
+
+export const register = async (prevState, formData) => {
+  // console.log("Hello, this is executiong on the server!");
+  // return {
+  //   skyColor: "blue",
+  //   grassColor: "green",
+  // };
+
+  const errors = {};
+
+  const ourUser = {
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+
+  if (typeof ourUser.username != "string") {
+    ourUser.username = "";
+  }
+  if (typeof ourUser.password != "string") {
+    ourUser.password = "";
+  }
+
+  ourUser.username = ourUser.username.trim();
+  ourUser.password = ourUser.password.trim();
+
+  if (ourUser.username.length < 3) {
+    errors.username = "Username must be at least 3 characters.";
+  }
+  if (ourUser.username.length > 30) {
+    errors.username = "Username cannot exceed 30 characters.";
+  }
+  if (!isAlphaNumeric(ourUser.username)) {
+    errors.username = "Username can only contain letters and numbers.";
+  }
+  if (ourUser.username == "") {
+    errors.username = "You must provide a username.";
+  }
+
+  if (ourUser.username.length < 12) {
+    errors.password = "Password must be at least 12 characters.";
+  }
+  if (ourUser.password.length > 50) {
+    errors.password = "Password cannot exceed 50 characters.";
+  }
+  if (ourUser.password == "") {
+    errors.password = "You must provide a Password.";
+  }
+
+  if (errors.username || errors.password) {
+    return {
+      errors: errors,
+      success: false,
+    };
+  }
+
+  // storing a new user in the database
+  // log the user in by giving them a cookie
+
+  return {
+    success: true,
+  };
+};
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import RegisterForm from "@/components/RegisterForm";
 
 const Login = () => {
   return (
@@ -6,17 +7,7 @@ const Login = () => {
       <p className="text-center text-2xl mb-5">
         Don&rsquo;t have an Account? <strong>Create One</strong>{" "}
       </p>
-      <form action="." className="max-w-xs mx-auto">
-        <div className="mb-3">
-          <input name="username" autoComplete="off" type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="mb-3">
-          <input name="password" autoComplete="off" type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Create Account
-        </button>
-      </form>
+      <RegisterForm />
     </>
   );
 };
